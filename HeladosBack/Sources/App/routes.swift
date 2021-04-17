d60ded53728226f9b334c73c9ed85a86e2f7a82f
Alejandro Hernández López <alexhl1999@hotmail.com>
2021-04-17 15:43:22 -0500

    Added controllers

--- HeladosBack/Sources/App/routes.swift
+++ HeladosBack/Sources/App/routes.swift
@@ -9,5 +9,30 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    let adminController = AdministratorController()
+    try app.register(collection: adminController)
+    
+    let productController = ProductController()
+    try app.register(collection: productController)
+    
+    let categoryController = CategoryController()
+    try app.register(collection: categoryController)
+    
+    let heladoController = HeladoController()
+    try app.register(collection: heladoController)
+    
+    let fotoProductoController = FotoProductoController()
+    try app.register(collection: fotoProductoController)
+    
+    let clientController = ClientController()
+    try app.register(collection: clientController)
+    
+    let clientUserController = ClientUserController()
+    try app.register(collection: clientUserController)
+    
+    let orderController = OrderController()
+    try app.register(collection: orderController)
+ 
 
 }
