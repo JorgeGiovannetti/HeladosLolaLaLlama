e06950936fed39e8891e4ca328ba8e3510497c5b
AlePestana <marialepestana@gmail.com>
2021-04-20 21:40:54 -0500

    Create NavBar

--- HeladosFront/src/components/general/Navbar.js
+++ HeladosFront/src/components/general/Navbar.js
@@ -14,11 +14,14 @@ import {
 	useDisclosure,
 	useColorModeValue,
 	Stack,
+	Icon,
+	Spacer,
+	Text,
 } from '@chakra-ui/react'
+import { HiShoppingCart } from 'react-icons/hi'
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
-import { useAuth } from '../../providers/AuthProvider'
 
-const Links = ['Dashboard', 'Products', 'Orders']
+// const Links = ['Helados', 'Helados Lola La Llama', 'Carrito']
 
 const NavLink = ({ link, children }) => (
 	<Link
@@ -27,11 +30,10 @@ const NavLink = ({ link, children }) => (
 		rounded={'md'}
 		_hover={{
 			textDecoration: 'none',
-			bg: useColorModeValue('purple.500', 'purple.700'),
+			bg: useColorModeValue('yellow.100', 'yellow.200'),
 		}}
-		href={`/admin/${link}`}
-		color={'white'}
-		fontWeight='semibold'
+		href={`/${link}`}
+		color={'gray.800'}
 	>
 		{children}
 	</Link>
@@ -39,68 +41,71 @@ const NavLink = ({ link, children }) => (
 
 const Navbar = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
-	const { logout } = useAuth()
-
-	const handleLogout = () => {
-		logout()
-			.then(() => {
-				console.log('Logged out!')
-			})
-			.catch((e) => console.log('error', e))
-	}
 
 	return (
 		<Box position={'fixed'} top={'0'} left={'0'}>
 			<Box
-				bg={useColorModeValue('purple.400', 'purple.900')}
+				// bg={useColorModeValue('yellow.100', 'yellow.200')}
 				px={4}
 				minW={'100vw'}
 			>
-				<Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+				<Flex
+					h={16}
+					alignItems={'center'}
+					justifyContent={'space-between'}
+					w={'full'}
+				>
 					<IconButton
 						size={'md'}
-						colorScheme='purple'
+						colorScheme='yellow'
 						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
 						aria-label={'Open Menu'}
 						display={{ md: !isOpen ? 'none' : 'inherit' }}
 						onClick={isOpen ? onClose : onOpen}
 					/>
-					<HStack spacing={8} alignItems={'center'}>
-						<HStack
+					<HStack spacing={8} alignItems={'center'} w={'full'}>
+						<Flex
 							as={'nav'}
 							spacing={4}
 							display={{ base: 'none', md: 'flex' }}
+							justifyContent={'space-between'}
+							w={'full'}
+							alignItems={'center'}
 						>
-							{Links.map((link) => (
-								<NavLink key={link} link={link}>
-									{link}
-								</NavLink>
-							))}
-						</HStack>
-					</HStack>
-					<Flex alignItems={'center'}>
-						<Menu>
-							<MenuButton
-								as={Button}
-								rounded={'full'}
-								variant={'link'}
-								cursor={'pointer'}
+							<NavLink key={'helados'} link={'Products'}>
+								Helados
+							</NavLink>
+							<Link
+								px={2}
+								py={1}
+								rounded={'md'}
+								href={'Landing'}
+								color={'gray.800'}
+								_hover={{
+									textDecoration: 'none',
+								}}
 							>
-								<Avatar size={'sm'} border={'2px'} color={'white'} />
-							</MenuButton>
-							<MenuList>
-								<MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
-							</MenuList>
-						</Menu>
-					</Flex>
+								<Text fontSize={'3xl'}>Helados Lola La Llama</Text>
+							</Link>
+							<NavLink key={'carrito'} link={'Cart'}>
+								<Icon w={6} h={6} color='gray.800' as={HiShoppingCart}></Icon>
+							</NavLink>
+						</Flex>
+					</HStack>
 				</Flex>
 
 				{isOpen ? (
 					<Box pb={4}>
 						<Stack as={'nav'} spacing={4}>
-							{Links.map((link) => (
-								<NavLink key={link}>{link}</NavLink>
-							))}
+							<NavLink key={'helados'} link={'Products'}>
+								Helados
+							</NavLink>
+							<NavLink key={'home'} link={'Landing'}>
+								Helados Lola La Llama
+							</NavLink>
+							<NavLink key={'carrito'} link={'Cart'}>
+								<Icon w={6} h={6} color='gray.800' as={HiShoppingCart}></Icon>
+							</NavLink>
 						</Stack>
 					</Box>
 				) : null}
