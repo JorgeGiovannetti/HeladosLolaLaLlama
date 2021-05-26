941182c937a7f38b542b066a29f10f24a9942bad
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-05-25 22:10:07 -0500

    Placeholder admin orders view

--- HeladosFront/src/views/admin/Orders.js
+++ HeladosFront/src/views/admin/Orders.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-import Navbar from "../../components/admin/Navbar";
+import React from 'react'
+import { Spinner } from '@chakra-ui/react'
+import Navbar from '../../components/admin/Navbar'
+import { Center, SimpleGrid } from '@chakra-ui/layout'
+import useOrders from '../../utils/hooks/useOrders'
 
 const Orders = () => {
+  const { orders, isLoading, error } = useOrders()
+
+  if (error) {
+    console.log('Error fetching orders', error)
+  }
+
+  const componentsChild = isLoading ? (
+    <Center>
+      <Spinner size='xl' color={'gray'} />
+    </Center>
+  ) : (
+    orders.map((order) => (
+      <div>{order.client?.name}</div>
+    ))
+  )
+
   return (
     <>
       <Navbar />
-      <Box>Orders</Box>
+      <SimpleGrid minChildWidth='360px' gap={12} w='100%' p='12' mt={12}>
+        {componentsChild}
+      </SimpleGrid>
     </>
-  );
-};
+  )
+}
 
 export default Orders;
