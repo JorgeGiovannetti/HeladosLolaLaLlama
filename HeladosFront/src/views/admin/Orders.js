931b0f71135212f25890aefdb75a41d70161b221
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-06-02 18:51:43 -0500

    Sort orders by date

--- HeladosFront/src/views/admin/Orders.js
+++ HeladosFront/src/views/admin/Orders.js
@@ -122,7 +122,7 @@ const OrdersAdmin = () => {
 
   const productsRows = isLoading
     ? null
-    : orders.map(({ id, total, paid, client, dateOfOrder }, key) => {
+    : orders.sort((a, b) => new Date(b.dateOfOrder) - new Date(a.dateOfOrder)).map(({ id, total, paid, client, dateOfOrder }, key) => {
         const orderDate = new Date(dateOfOrder);
 
         return (
